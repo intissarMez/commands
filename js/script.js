bf5d7c9d3a9dde73909bb8ba4e206d0a63dd9a8a
intissarMez <intissar.elmezrouai@gmail.com>
2016-05-10 17:17:13 +0100

    Shortcut to open the palette + keyboard up/down + execute command by hitting enter

--- js/script.js
+++ js/script.js
@@ -1,42 +1,80 @@
-angular.module('cmdPalette', ['ngSanitize'])
+var extension = angular.module('cmdPalette', ['ngSanitize']);
 
-/*Disabling Debug Data*/
-.config(['$compileProvider', function ($compileProvider) {
-  $compileProvider.debugInfoEnabled(false);
-}])
+  /*Disabling Debug Data*/
+  extension.config(['$compileProvider', function ($compileProvider) {
+    $compileProvider.debugInfoEnabled(false);
+  }]);
 
-.controller('MainController', function($scope, $http, $filter) {
-    $http.get('js/builtInCmd.json').success(function(data) {
-      $scope.commands = data;
-      $scope.filterData = function (query){
-      $scope.commands = $filter('filter')(data, query)};
-      }).error(function(error) {
+  extension.controller('MainController', function ($scope, $http, $filter) {
+    var data         = [];
+    var currentItems = [];
+    var txtInput = document.getElementById("queryInput");
+    $scope.selectedCommand = 0;
+
+    var exec = function(arg){
         studio.extension.quitDialog();
+        studio.sendCommand(arg)
+    };
+
+    var filterData = function(query) {
+      $scope.commands     = $filter('filter')(data, query);
+      currentItems        = document.getElementsByClassName("list-group-item");
+      $scope.selectedCommand = 0;
+    };
+
+
+    $http.get('js/builtInCmd.json').success(function (response) {
+      $scope.commands = data = response;
+    }).error(function (error) {
+      studio.extension.quitDialog();
     });
 
-    $scope.closeDialog = function($event) {
+    $scope.closeDialog = function ($event) {
 
-        if (event.keyCode === 27) {
-            studio.extension.quitDialog();
-        }
+      if (event.keyCode === 27) {
+        studio.extension.quitDialog();
+      }
+    };
+
+   $scope.filterDataResult = function (event) {
+      filterData($scope.query);
+    };
+
+    $scope.sendCommand = function (cmd) {
+        exec(cmd);
+    };
+
+
+   txtInput.onkeydown = function (event) {
+      var currentCommand = document.getElementById('command-' +$scope.selectedCommand).value;
+      switch (event.keyIdentifier) {
+        case 'Enter':
+          exec(currentCommand);
+          break;
+        case "Up":
+          event.preventDefault();
+          $scope.selectedCommand && $scope.selectedCommand--;
+          break;
+        case "Down":
+          event.preventDefault();
+          ($scope.selectedCommand < currentItems.length - 1 ) && $scope.selectedCommand++;
+          break;
+      }
     }
+    filterData("");
 
-    $scope.sendCommand = function(cmd) {
-      studio.extension.quitDialog();
-      studio.sendCommand(cmd);
-    }
-})
+  });
 
 
-.filter('commandKey', function () {
-  return function (input) {
-      return input.replace('key', studio.os.isWindows ? 'ctrl':'cmd');
-  };
-})
+  extension.filter('commandKey', function () {
+    return function (input) {
+      return input.replace('key', studio.os.isWindows ? 'ctrl' : 'cmd');
+    };
+  })
 
-.filter('highlight', function( $sanitize) {
-    return function(input, selectedWord) {
-      if(selectedWord) {
+  extension.filter('highlight', function ($sanitize) {
+    return function (input, selectedWord) {
+      if (selectedWord) {
         var pattern = new RegExp(selectedWord, "gi");
         return input.replace(pattern, $sanitize("<span class='highlighted'>" + selectedWord + "</span>"));
       }
@@ -44,4 +82,4 @@ angular.module('cmdPalette', ['ngSanitize'])
         return input;
       }
     };
-});
+  });
